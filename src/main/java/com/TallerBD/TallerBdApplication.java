9ec0f87b8022f9f076a111cf360b89594c40b739
JerryezerTorres <jerryezert@gmail.com>
2023-03-04 20:30:02 -0400

    entrega prueba 1

---
+++ src/main/java/com/TallerBD/TallerBdApplication.java
@@ -0,0 +1,13 @@
+package com.TallerBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TallerBD/TallerBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TallerBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
